e4325e6f125c4a3bbf22da7da5242755e155de05
Rangoli Panwar <143282697+rangolipanwar@users.noreply.github.com>
2023-09-22 11:32:34 +0530

    Update database.sql

--- database.sql
+++ database.sql
@@ -1,1 +1,10 @@
-
+CREATE TABLE birthday_data (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(50),
+    middle_name VARCHAR(50),
+    last_name VARCHAR(50),
+    year INT,
+    month INT,
+    day INT,
+    sms VARCHAR(1)
+);
